48028d6f38600494ad9b6c08cc6c9597cd659209
iserhat <iserhatercan@gmail.com>
2024-05-11 22:23:09 -0400

    comments are added in main.py

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ job = Job("Software Developer", "New Jersey", "developer.csv")
 JOB_ROLE = job.role
 STATE = job.state
 FILE = job.file
+JOBS_PER_PAGE = 15
 
 opt = webdriver.ChromeOptions()
 opt.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=opt)
 driver.delete_all_cookies()
 
+#Getting the indeed index page and searching specified job and location
 url = 'https://www.indeed.com/'
 driver.get(url=url)
 job_title = driver.find_element(By.NAME, value="q")
@@ -28,14 +30,13 @@ location.send_keys(Keys.CONTROL + "a")
 location.send_keys(STATE, Keys.ENTER)
 time.sleep(0.5)
 
-
+#Calculating the number of pages for specified job and location
+#The num_of_page can be used for getting all search results
 job_count = driver.find_element(By.CSS_SELECTOR, '.jobsearch-JobCountAndSortPane-jobCount span').text
 job_count = job_count.replace(',','')
 count = re.match('([0-9]*)', job_count)
 number_of_job = int(count[0])
-JOBS_PER_PAGE = 15
 num_of_page = int(number_of_job/JOBS_PER_PAGE)
-
 jobs = []
 
 def get_list():
@@ -44,9 +45,6 @@ def get_list():
         infos = []
         title = result.find_element(By.XPATH, ".//span").text     
         childs = result.find_elements(By.XPATH, ".//*")
-        #specs = {}
-        
-        #specs['title'] = title
         infos.append(title)
         for child in childs:
             name = child.get_attribute('data-testid')
@@ -60,20 +58,20 @@ def get_list():
                 infos.append(child.text)
             else:
                 continue
-        #specs['info'] = infos
-        #jobs.append(specs)
         jobs.append(infos)
 
 get_list()
 
-for i in range(10):
+
+# the range of the "for loop" choosed as 5 for testing purpose. num_of_page can be used for getting all search results 
+for i in range(5):
     pages = driver.find_elements(By.CSS_SELECTOR, 'nav ul li a')
     new_url = pages[-1].get_attribute('href')
     driver.get(url=new_url)
     time.sleep(2)
     get_list()
-df = pd.DataFrame(jobs, columns=['jobTitle','company','location','salary','shift','workTime', 'extra'])
 
+df = pd.DataFrame(jobs, columns=['jobTitle','company','location','salary','shift','workTime', 'extra'])
 df.to_csv(FILE)
     
 
